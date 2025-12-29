152ee1740291256b0245fb18edb6666e0062dfbd
CUBoulder-Ashwin <cuboulder-ashwin@gmail.com>
2025-12-28 21:49:44 -0800

    Initial commit

--- Samples/test_ocr.py
+++ Samples/test_ocr.py
@@ -1,6 +1,6 @@
 import easyocr
 
 reader = easyocr.Reader(['en'], gpu=False)
-ocr_result = reader.readtext('bestbuy.png', detail=0)
+ocr_result = reader.readtext('banana_republic.png', detail=0)
 
 print(ocr_result)
